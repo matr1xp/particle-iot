5567a67c3ab3911ca788e4428cef5e71c6b1303d
Marlon Santos <learn@marlsantos.com>
2017-03-20 12:24:41 +1100

    Add resource bme280-oled resources, misc header files

---
+++ src/settings.h
@@ -0,0 +1,4 @@
+#define REFRESH_INTERVAL              (30000)
+#define BME280_ADDRESS                (0x76)
+#define SSD1306_ADDRESS               (0x3C)
+#define SEALEVELPRESSURE_HPA          (1013.25)
